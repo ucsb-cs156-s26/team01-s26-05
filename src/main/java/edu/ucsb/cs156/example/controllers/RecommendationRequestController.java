e848612da5d6854e5d0321dd61933c2692634877
Katie Pyo <soome@ucsb.edu>
2026-04-28 18:23:57 -0700

    We added /api/recommendationRequests/all and /api/recommendationRequests/post endpoint for all recommendation requests, and creating a single recommendation request

---
+++ src/main/java/edu/ucsb/cs156/example/controllers/RecommendationRequestController.java
@@ -0,0 +1,93 @@
+package edu.ucsb.cs156.example.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import edu.ucsb.cs156.example.entities.RecommendationRequest;
+import edu.ucsb.cs156.example.repositories.RecommendationRequestRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.time.LocalDateTime;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/* This is a REST controller for RecommendationRequests */
+@Tag(name = "RecommendationRequest")
+@RequestMapping("/api/recommendationRequest")
+@RestController
+@Slf4j
+public class RecommendationRequestController extends ApiController {
+  @Autowired RecommendationRequestRepository recommendationRequestRepository;
+
+  /**
+   * THis method returns a list of all Recommendation requests.
+   *
+   * @return List all recommendation dates
+   */
+  @Operation(summary = "List all recommendation requests")
+  @PreAuthorize("hasRole('ROLE_USER')")
+  @GetMapping("/all")
+  public Iterable<RecommendationRequest> allRecommendationRequests() {
+    Iterable<RecommendationRequest> recommendationRequests =
+        recommendationRequestRepository.findAll();
+    return recommendationRequests;
+  }
+
+  /**
+   * Create a new recommendation request
+   *
+   * @param requesterEmail the email of requester
+   * @param professorEmail the email of professor
+   * @param explanation the explanation of request
+   * @param dateRequested the date of request
+   * @param dateNeeded the date of recommendation needed by
+   * @param done whether recommendation was done
+   * @return the saved recommendation request
+   */
+  @Operation(summary = "Create a new recommendation request")
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
+  @PostMapping("/post")
+  public RecommendationRequest postRecommendationRequest(
+      @Parameter(name = "requesterEmail") @RequestParam String requesterEmail,
+      @Parameter(name = "professorEmail") @RequestParam String professorEmail,
+      @Parameter(name = "explanation") @RequestParam String explanation,
+      @Parameter(
+              name = "dateRequested",
+              description =
+                  "date requested (in iso format, e.g. YYYY-mm-ddTHH:MM:SS; see https://en.wikipedia.org/wiki/ISO_8601)")
+          @RequestParam("dateRequested")
+          @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+          LocalDateTime dateRequested,
+      @Parameter(
+              name = "dateNeeded",
+              description =
+                  "date needed (in iso format, e.g. YYYY-mm-ddTHH:MM:SS; see https://en.wikipedia.org/wiki/ISO_8601)")
+          @RequestParam("dateNeeded")
+          @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+          LocalDateTime dateNeeded,
+      @Parameter(name = "done") @RequestParam boolean done)
+      throws JsonProcessingException {
+
+    log.info("dateRequested={}", dateRequested);
+    log.info("dateNeeded={}", dateNeeded);
+
+    RecommendationRequest recommendationRequest = new RecommendationRequest();
+    recommendationRequest.setRequesterEmail(requesterEmail);
+    recommendationRequest.setProfessorEmail(professorEmail);
+    recommendationRequest.setExplanation(explanation);
+    recommendationRequest.setDateNeeded(dateNeeded);
+    recommendationRequest.setDateRequested(dateRequested);
+    recommendationRequest.setDone(done);
+
+    RecommendationRequest savedRecommendationRequest =
+        recommendationRequestRepository.save(recommendationRequest);
+
+    return savedRecommendationRequest;
+  }
+}
